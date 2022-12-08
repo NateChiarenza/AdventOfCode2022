b42c66237eda8bc5c726b77966c2dc2cc25535ae
NateChiarenza <74154204+NateChiarenza@users.noreply.github.com>
2022-12-08 15:42:16 -0500

    Add files via upload

---
+++ Advent Of Webapps/Controllers/Advent8.cs
@@ -0,0 +1,33 @@
+﻿using Advent_Of_Webapps.answers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Advent_Of_Webapps.Controllers
+{
+    [Route("api/advent8")]
+    [ApiController]
+    public class Advent8 : ControllerBase
+    {
+        private readonly IAdvent8Service _advent8;
+
+        public Advent8(IAdvent8Service a)
+        {
+            _advent8 = a;
+        }
+
+        [HttpPost("Part1")]
+        public async Task<IActionResult> part1(IFormFile input)
+        {
+            var result = await _advent8.part1(input);
+            return Ok(result);
+        }
+        [HttpPost("Part2")]
+        public async Task<IActionResult> part2(IFormFile input)
+        {
+            var result = await _advent8.part2(input);
+            return Ok(result);
+        }
+
+    }
+}
